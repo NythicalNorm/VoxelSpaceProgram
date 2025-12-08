4a161c2432daeb9d82270bf11665aa9d19f1345c
NythicalNorm <cephandriushoid@outlook.com>
2025-12-09 04:27:38 +0530

    Hyperbolic, parabolic equations and their positions and velocities mostly work, only Hyperbolic Eccentricity formulation in the fromCartesian method is wrong. semi major axis conversion also fixed.

--- src/test/java/com/nythicalnorm/nythicalSpaceProgram/orbit/OrbitalElementsTest.java
+++ src/test/java/com/nythicalnorm/nythicalSpaceProgram/orbit/OrbitalElementsTest.java
@@ -17,45 +17,106 @@ class OrbitalElementsTest {
 
 
    @Test
-   void conversionTest() {
-       elements.setOrbitalPeriod(sunMass);
-       elementsNila.setOrbitalPeriod(5.972E24);
+   void conversionTest1() {
+       //elements.setOrbitalPeriod(sunMass);
+       elementsNila.setOrbitalPeriod(5.97219E24);
        long currentnanotime = System.nanoTime();
 
        for (int i = 0; i < 100; i++) {
            double timeElapsed = i*1000000;
-           Vector3d[] stateVectors = elements.ToCartesian(timeElapsed);
+           Vector3d[] stateVectors = elementsNila.ToCartesian(timeElapsed);
            Vector3d pos = stateVectors[0];
            Vector3d vel = stateVectors[1];
 
-           OrbitalElements orbitFromState = new OrbitalElements(pos, vel, timeElapsed, sunMass);
+           OrbitalElements orbitFromState = new OrbitalElements(pos, vel, timeElapsed, 5.97219E24);
 
            System.out.println("---------------" + timeElapsed + "---------------");
 
-           double semiMajor = Math.abs(elements.SemiMajorAxis - orbitFromState.SemiMajorAxis);
+           double semiMajor = Math.abs(elementsNila.SemiMajorAxis - orbitFromState.SemiMajorAxis);
            System.out.println("Semi Major Axis Diff: " + semiMajor);
 
-           double eccentricity = Math.abs(elements.Eccentricity - orbitFromState.Eccentricity);
+           double eccentricity = Math.abs(elementsNila.Eccentricity - orbitFromState.Eccentricity);
            System.out.println("Eccentricity Diff: " + eccentricity);
 
-           double inclination = Math.abs(elements.Inclination - orbitFromState.Inclination);
+           double inclination = Math.abs(elementsNila.Inclination - orbitFromState.Inclination);
            System.out.println("Inclination Diff: " + inclination);
 
-           double argumentofperi = Math.abs(elements.ArgumentOfPeriapsis - orbitFromState.ArgumentOfPeriapsis);
+           double argumentofperi = Math.abs(elementsNila.ArgumentOfPeriapsis - orbitFromState.ArgumentOfPeriapsis);
            System.out.println("Argument Of Periapsis Diff: " + argumentofperi);
 
-           double longitudeoftheascendingnode = Math.abs(elements.LongitudeOfAscendingNode - orbitFromState.LongitudeOfAscendingNode);
+           double longitudeoftheascendingnode = Math.abs(elementsNila.LongitudeOfAscendingNode - orbitFromState.LongitudeOfAscendingNode);
            System.out.println("Longitude Of Ascending Node Diff: " + longitudeoftheascendingnode);
 
-           double meanangularmotion = Math.abs(elements.MeanAngularMotion - orbitFromState.MeanAngularMotion);
+           double meanangularmotion = Math.abs(elementsNila.MeanAngularMotion - orbitFromState.MeanAngularMotion);
            System.out.println("Mean Angular Motion Diff: " + meanangularmotion);
 
-           double periapsistime = Math.abs(elements.periapsisTime - orbitFromState.periapsisTime);
+           double periapsistime = Math.abs(elementsNila.periapsisTime - orbitFromState.periapsisTime);
            System.out.println("periapsis Time Diff: " + periapsistime);
        }
 
        long timepassed = System.nanoTime() - currentnanotime;
        System.out.println("took: " + (double)timepassed/1000000);
+   }
+
+    @Test
+    void conversionTest2() {
+        long currentnanotime = System.nanoTime();
+
+        Vector3d posInit = new Vector3d(-2.9936153959992264E10, -606297.1566449205, 1.4402476049850812E11);
+        Vector3d velInit = new Vector3d(-29659.438335282175, 0.06573780798998548, -6164.840027386336);
+
+        OrbitalElements orbitFromState = new OrbitalElements(posInit, velInit, 0, sunMass);
+
+        double timeElapsed = 0;
+        Vector3d[] stateVectors = orbitFromState.ToCartesian(timeElapsed);
+        Vector3d pos = stateVectors[0];
+        Vector3d vel = stateVectors[1];
+
+        long timepassed = System.nanoTime() - currentnanotime;
+        System.out.println("took: " + (double)timepassed/1000000);
+    }
+
+    @Test
+    void HyperbolicOrbitTest() {
+        long currentnanotime = System.nanoTime();
+        Vector3d position = new Vector3d(6871000,50,5);
+        Vector3d velocity = new Vector3d(5, 5, 12000);
+
+        OrbitalElements orbitFromState = new OrbitalElements(position, velocity, 0, 5.972E24);
+        System.out.println("--------------- Original ---------------");
+        System.out.println("posX: " + position.x + " posY: " + position.y + " posZ: " + position.z);
+        System.out.println("velX: " + velocity.x + " velY: " + velocity.y + " velZ: " + velocity.z);
+        System.out.println("Eccentricity: " + orbitFromState.Eccentricity);
+
+        for (int i = 0; i < 100; i++) {
+            double time = i*1000;
+
+            Vector3d[] stateVectors = orbitFromState.ToCartesian(time);
+            Vector3d pos = stateVectors[0];
+            Vector3d vel = stateVectors[1];
+
+            System.out.println("---------------" + time + "---------------");
+            System.out.println("posX: " + pos.x + " posY: " + pos.y + " posZ: " + pos.z);
+            System.out.println("distance: " + pos.length());
+            System.out.println("velX: " + vel.x + " velY: " + vel.y + " velZ: " + vel.z);
+            System.out.println("Speed: " + vel.length());
+        }
+    }
+
+   @Test
+   void customConversionTest() {
+       Vector3d position = new Vector3d(6871000,50,0);
+       Vector3d velocity = new Vector3d(0, 0, 7000);
+       double timeElapsed = 0;
+
+       OrbitalElements elem = new OrbitalElements(position, velocity, timeElapsed, 5.972E24);
+
+       Vector3d[] stateVectors = elem.ToCartesian(timeElapsed);
+       Vector3d pos = stateVectors[0];
+       Vector3d vel = stateVectors[1];
+
+       System.out.println("posX: " + pos.x + " posY: " + pos.y + " posZ: " + pos.z);
+       System.out.println("velX: " + vel.x + " velY: " + vel.y + " velZ: " + vel.z);
    }
 
    @Test
@@ -78,13 +139,16 @@ class OrbitalElementsTest {
     void StateVectorToOrbitalElements() {
         elements.setOrbitalPeriod(sunMass);
         long currentnanotime = System.nanoTime();
+        Vector3d posInit = new Vector3d(-2.9936153959992264E10, -606297.1566449205, 1.4402476049850812E11);
+        Vector3d velInit = new Vector3d(-29659.438335282175, 0.06573780798998548, -6164.840027386336);
+
+        OrbitalElements orbitFromState = new OrbitalElements(posInit, velInit, 0, sunMass);
 
         for (int i = 0; i < 1000000; i++) {
             double timeElapsed = i*100000;
             Vector3d pos = new Vector3d(-2.9936153959992264E10, -606297.1566449205, 1.4402476049850812E11);
             Vector3d vel = new Vector3d(-29659.438335282175, 0.06573780798998548, -6164.840027386336);
-
-            OrbitalElements orbitFromState = new OrbitalElements(pos, vel, timeElapsed, sunMass);
+            orbitFromState.fromCartesian(pos, vel, timeElapsed);
         }
 
         long timepassed = System.nanoTime() - currentnanotime;
